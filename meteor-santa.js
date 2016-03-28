7dbd87e4ead4f95e2a362aab149fd231b9416c73
Paul Cuciureanu <paulcu@gmail.com>
2016-03-28 10:22:25 -0400

    Starting off from a real-time user status demo: https://github.com/mizzao/meteor-user-status/tree/master/demo - we can implement #27 from here

--- meteor-santa.js
+++ meteor-santa.js
@@ -1,23 +1,139 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  userConnections = new Mongo.Collection("user_status_sessions");
+  relativeTime = function(timeAgo) {
+    var ago, days, diff, time;
+    diff = moment.utc(TimeSync.serverTime() - timeAgo);
+    time = diff.format("H:mm:ss");
+    days = +diff.format("DDD") - 1;
+    ago = (days ? days + "d " : "") + time;
+    return ago + " ago";
+  };
+  Handlebars.registerHelper("userStatus", UserStatus);
+  Handlebars.registerHelper("localeTime", function(date) {
+    return date != null ? date.toLocaleString() : void 0;
+  });
+  Handlebars.registerHelper("relativeTime", relativeTime);
+  Template.login.helpers({
+    loggedIn: function() {
+      return Meteor.userId();
     }
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.status.events = {
+    "submit form.start-monitor": function(e, tmpl) {
+      e.preventDefault();
+      return UserStatus.startMonitor({
+        threshold: tmpl.find("input[name=threshold]").valueAsNumber,
+        interval: tmpl.find("input[name=interval]").valueAsNumber,
+        idleOnBlur: tmpl.find("select[name=idleOnBlur]").value === "true"
+      });
+    },
+    "click .stop-monitor": function() {
+      return UserStatus.stopMonitor();
+    },
+    "click .resync": function() {
+      return TimeSync.resync();
     }
+  };
+  Template.status.helpers({
+    lastActivity: function() {
+      var lastActivity;
+      lastActivity = this.lastActivity();
+      if (lastActivity != null) {
+        return relativeTime(lastActivity);
+      } else {
+        return "undefined";
+      }
+    }
+  });
+  Template.status.helpers({
+    serverTime: function() {
+      return new Date(TimeSync.serverTime()).toLocaleString();
+    },
+    serverOffset: TimeSync.serverOffset,
+    serverRTT: TimeSync.roundTripTime,
+    isIdleText: function() {
+      return this.isIdle() || "false";
+    },
+    isMonitoringText: function() {
+      return this.isMonitoring() || "false";
+    }
+  });
+  Template.serverStatus.helpers({
+    anonymous: function() {
+      return UserConnections.find({
+        userId: {
+          $exists: false
+        }
+      });
+    },
+    users: function() {
+      return Meteor.users.find();
+    },
+    userClass: function() {
+      var ref;
+      if ((ref = this.status) != null ? ref.idle : void 0) {
+        return "warning";
+      } else {
+        return "success";
+      }
+    },
+    connections: function() {
+      return UserConnections.find({
+        userId: this._id
+      });
+    }
+  });
+  Template.serverConnection.helpers({
+    connectionClass: function() {
+      if (this.idle) {
+        return "warning";
+      } else {
+        return "success";
+      }
+    },
+    loginTime: function() {
+      if (this.loginTime == null) {
+        return;
+      }
+      return new Date(this.loginTime).toLocaleString();
+    }
+  });
+  Template.login.events = {
+    "submit form": function(e, tmpl) {
+      var input;
+      e.preventDefault();
+      input = tmpl.find("input[name=username]");
+      input.blur();
+      return Meteor.insecureUserLogin(input.value, function(err, res) {
+        if (err) {
+          return console.log(err);
+        }
+      });
+    }
+  };
+  Deps.autorun(function(c) {
+    try {
+      UserStatus.startMonitor({
+        threshold: 30000,
+        idleOnBlur: true
+      });
+      return c.stop();
+    } catch (_error) {}
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  process.env.HTTP_FORWARDED_COUNT = 1;
+  Meteor.publish(null, function() {
+    return [
+      Meteor.users.find({
+        "status.online": true
+      }, {
+        fields: {
+          status: 1,
+          username: 1
+        }
+      }), UserStatus.connections.find()
+    ];
   });
 }
