1753429d83c3ac63263b39eef69076fdb6e35f29
Paul Cuciureanu <paulcu@gmail.com>
2016-03-28 13:51:14 -0400

    Adding initial matching algorithm, everyone is their own santa #2 #3 and #6

--- meteor-santa.js
+++ meteor-santa.js
@@ -1,6 +1,5 @@
 if (Meteor.isClient) {
   UserConnections = new Mongo.Collection("user_status_sessions");
-
   relativeTime = function(timeAgo) {
     var ago, days, diff, time;
     diff = moment.utc(TimeSync.serverTime() - timeAgo);
@@ -19,6 +18,7 @@ if (Meteor.isClient) {
       return Meteor.userId();
     }
   });
+
   Template.status.events = {
     "submit form.start-monitor": function(e, tmpl) {
       e.preventDefault();
@@ -35,6 +35,7 @@ if (Meteor.isClient) {
       return TimeSync.resync();
     }
   };
+
   Template.status.helpers({
     lastActivity: function() {
       var lastActivity;
@@ -46,6 +47,7 @@ if (Meteor.isClient) {
       }
     }
   });
+
   Template.status.helpers({
     serverTime: function() {
       return new Date(TimeSync.serverTime()).toLocaleString();
@@ -59,6 +61,20 @@ if (Meteor.isClient) {
       return this.isMonitoring() || "false";
     }
   });
+
+  Template.Santas.helpers({
+    users: function() {
+      return Meteor.users.find();
+    },
+    giftee: function() {
+      if (Meteor.user() && Meteor.user().gifteeId) {
+        return Meteor.users.findOne({_id: Meteor.user().gifteeId});
+      } else {
+        return null;
+      }
+    }
+  });
+
   Template.Hat.helpers({
     users: function() {
       return Meteor.users.find();
@@ -104,6 +120,15 @@ if (Meteor.isClient) {
       return new Date(this.loginTime).toLocaleString();
     }
   });
+
+  Template.Santas.events = {
+    "submit form": function(e, tmpl) {
+      e.preventDefault();
+      Meteor.call("matchSantas");
+      console.log("Santas button pressed!");
+    }
+  };
+
   Template.Login.events = {
     "submit form": function(e, tmpl) {
       var input;
@@ -129,6 +154,20 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+  /**
+   * Randomize array element order in-place.
+   * Using Durstenfeld shuffle algorithm.
+   */
+  function shuffleArray(array) {
+      for (var i = array.length - 1; i > 0; i--) {
+          var j = Math.floor(Math.random() * (i + 1));
+          var temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+      }
+      return array;
+  }
+
   process.env.HTTP_FORWARDED_COUNT = 1;
   Meteor.publish(null, function() {
     return [
@@ -137,9 +176,27 @@ if (Meteor.isServer) {
       }, {
         fields: {
           status: 1,
+          gifteeId: 1,
           username: 1
         }
       }), UserStatus.connections.find()
     ];
+  });
+
+  Meteor.methods({
+    matchSantas: function (taskId, setChecked) {
+      var players = Meteor.users.find().fetch();
+      var giftees = shuffleArray(Meteor.users.find().fetch());
+      _.each(players, function(player) {
+        // can't pick yourself, ensure gifteeId is not player's _id
+        gifteeId = player._id;
+        Meteor.users.update(player._id, {
+          $set: {
+            gifteeId: gifteeId
+          }
+        });
+        console.log(player.username, "gifts", Meteor.users.findOne({_id: gifteeId}).username);
+      });
+    }
   });
 }
