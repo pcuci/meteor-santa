e5e0fe68863105f1b749c0e9e5ad3621b93b93e8
Paul Cuciureanu <paulcu@gmail.com>
2016-04-03 11:49:39 -0400

    Removing single question in favour of inferred selection

--- client/templates/profile/profile.js
+++ client/templates/profile/profile.js
@@ -1,33 +1,15 @@
-Template.Profile.helpers({
-  single: function() {
-    if (Meteor.user()) {
-      if (Meteor.user().single) {
-        return 'checked';
-      } else {
-        return '';
-      }
-    }
-    return '';
-  },
-  conjoint: function() {
-    if (Meteor.user()) {
-      if (Meteor.user().single) {
-        return '';
-      } else {
-        return 'checked';
-      }
-    }
-    return '';
-  },
-  isConjoint: function() {
-    return (Meteor.user()) ? !Meteor.user().single : false;
+Template.Profile.events({
+  "change #username-select": function (event, template) {
+    var username = $(event.currentTarget).val();
+    console.log("username: " + username);
+    Meteor.call("setSweetheart", username);
   }
 });
 
-Template.Profile.events({
-  'change input[type=radio]': function (event, template) {
-    single = template.find('input:radio[name=single]:checked').value;
-    Meteor.call("setSingle", (single == "true"));
-    Meteor.call("setSweetheart", undefined);
+Template.Profile.helpers({
+  usernames: function() {
+    var users = Meteor.users.find({_id: {$ne: Meteor.userId()}}).fetch();
+    var usernames = _.pluck(users, 'username');
+    return usernames
   }
 });
