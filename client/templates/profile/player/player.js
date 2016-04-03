345415bbab19ab0de0130a358a1d6f027bc0c504
Paul Cuciureanu <paulcu@gmail.com>
2016-04-03 12:59:47 -0400

    Refactoring UI to indicate significant others #3, also covers #27 (no more attendees list, ensures you can't pic yourself #2, and still keeps track of single status #28

--- client/templates/profile/player/player.js
+++ client/templates/profile/player/player.js
@@ -1,5 +1,13 @@
 Template.Player.helpers({
   active: function() {
     return Meteor.user().sweetheart == this ? "active" : "";
+  },
+  match: function() {
+    var player = Meteor.users.findOne({"username": "" + this});
+    if (player) {
+      return Meteor.user().gifteeId == player._id ? "list-group-item-danger" : "";
+    } else {
+      return "";
+    }
   }
 });
